83a06e8f9578044adcd15c717bdbfc55946a02cc
Ed Peters <epeters1111@gmail.com>
2023-03-01 20:25:37 -0500

    Adds sign flipping on rotation

--- src/main/java/frc/robot/commands/swerve/SwerveTeleopCommand.java
+++ src/main/java/frc/robot/commands/swerve/SwerveTeleopCommand.java
@@ -2,15 +2,23 @@ package frc.robot.commands.swerve;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.swerve.SwerveDriveSubsystem;
 
 public class SwerveTeleopCommand extends CommandBase {
 
+    public enum FlipSign {
+        YES,
+        NO,
+        DONT_CARE
+    }
+
     private final SwerveDriveSubsystem swerveDrive;
     private final DoubleSupplier pxSupplier;
     private final DoubleSupplier pySupplier;
     private final DoubleSupplier pomegaSupplier;
+    private FlipSign flipSign;
 
     public SwerveTeleopCommand(SwerveDriveSubsystem swerveDrive,
                                DoubleSupplier pxSupplier,
@@ -21,15 +29,35 @@ public class SwerveTeleopCommand extends CommandBase {
         this.pxSupplier = pxSupplier;
         this.pySupplier = pySupplier;
         this.pomegaSupplier = pomegaSupplier;
+        this.flipSign = FlipSign.DONT_CARE;
 
         addRequirements(swerveDrive);
     }
 
     @Override
     public void execute() {
-        swerveDrive.drive(
-                pxSupplier.getAsDouble(),
-                pySupplier.getAsDouble(),
-                pomegaSupplier.getAsDouble());
+
+        double px = pxSupplier.getAsDouble();
+        double py = pySupplier.getAsDouble();
+        double pomega = pomegaSupplier.getAsDouble();
+
+        if (pomega == 0.0) {
+            flipSign = FlipSign.DONT_CARE;
+        }
+
+        if (flipSign == FlipSign.DONT_CARE) {
+            Rotation2d heading = swerveDrive.getYaw();
+            if (Math.abs(heading.getDegrees()) < 90) {
+                flipSign = FlipSign.YES;
+            } else {
+                flipSign = FlipSign.NO;
+            }
+        }
+
+        if (flipSign == FlipSign.YES) {
+            pomega = -pomega;
+        }
+
+        swerveDrive.drive(px, py, pomega);
     }
 }
