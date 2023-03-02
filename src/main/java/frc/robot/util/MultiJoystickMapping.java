a48df9fe8003c7c05e7936a9453de0ca8918d787
Ed Peters <epeters1111@gmail.com>
2023-03-01 21:10:53 -0500

    Fix joystick mapping issues

--- src/main/java/frc/robot/util/MultiJoystickMapping.java
+++ src/main/java/frc/robot/util/MultiJoystickMapping.java
@@ -70,7 +70,7 @@ public class MultiJoystickMapping {
     }
 
     public double getArmExtend() {
-        return -getArmAxis(XboxController.Axis.kRightX.value);
+        return -getArmAxis(XboxController.Axis.kRightY.value);
     }
 
     private double getArmAxis(int axis) {
