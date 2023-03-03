d43f8b2be224b18f2b703fd4b0400fd62605bba3
crwright77 <98225554+crwright77@users.noreply.github.com>
2023-03-02 19:46:29 -0500

    Mapping & Modification

--- src/main/java/frc/robot/RobotControlMapping.java
+++ src/main/java/frc/robot/RobotControlMapping.java
@@ -3,13 +3,17 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.arm.ArmPresetCommand;
+import frc.robot.commands.swerve.AlignToDegreesCommand;
 import frc.robot.commands.swerve.AlignToWallCommand;
+import frc.robot.commands.swerve.ParkingOnThePlatformCommand;
 import frc.robot.commands.swerve.RelativeTrajectoryCommand;
 import frc.robot.commands.swerve.SwerveCommands;
 import frc.robot.commands.swerve.SwerveTeleopCommand;
+import frc.robot.commands.swerve.AlignToWallCommand.Wall;
 import frc.robot.commands.HandCommands;
 import frc.robot.commands.arm.ArmCalibrationCommand;
 import frc.robot.commands.arm.ArmTeleopCommand;
@@ -86,7 +90,17 @@ public class RobotControlMapping {
         // buttons
         driver.start().onTrue(SwerveCommands.zeroGyro(drive));
         driver.leftStick().onTrue(SwerveCommands.turnWheels(drive, 90));
-        driver.x().onTrue(SwerveCommands.scootForward(drive, 6.0));
+        // driver.x().onTrue(SwerveCommands.scootForward(drive, 6.0));
+
+
+
+        driver.x().onTrue(Commands.sequence(
+            new AlignToDegreesCommand(drive, 0), 
+            new ParkingOnThePlatformCommand(drive),
+            SwerveCommands.turnWheels(drive, 90)
+        ));
+
+
         driver.b().onTrue(HandCommands.grab(hand));
         driver.a().onTrue(HandCommands.release(hand));
         driver.back().onTrue(new InstantCommand(() -> CommandScheduler.getInstance().cancelAll()));
