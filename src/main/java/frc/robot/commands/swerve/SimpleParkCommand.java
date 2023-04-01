850a9d762e20e1804a6ff2062f06f8252a01e7e9
Ed Peters <epeters1111@gmail.com>
2023-04-01 11:42:38 -0400

    Updates SimpleParkCommand

--- src/main/java/frc/robot/commands/swerve/SimpleParkCommand.java
+++ src/main/java/frc/robot/commands/swerve/SimpleParkCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.swerve;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.swerve.SwerveDriveSubsystem;
 
@@ -15,29 +16,42 @@ public class SimpleParkCommand extends CommandBase {
     private final SwerveDriveSubsystem drive;
     private double startingPitch;
     private double currentPitch;
+    private double deltaPitch;
     private boolean done;
 
     public SimpleParkCommand(SwerveDriveSubsystem drive) {
 
         this.drive = drive;
 
+        SmartDashboard.putData("SimpleParkCommand", builder -> {
+            builder.addBooleanProperty("Done", () -> done, null);
+            builder.addDoubleProperty("PitchCurrent", () -> currentPitch, null);
+            builder.addDoubleProperty("PitchDelta", () -> deltaPitch, null);
+            builder.addDoubleProperty("PitchStart", () -> startingPitch, null);
+        });
+
         addRequirements(drive);
     }
 
     public void initialize() {
         this.startingPitch = drive.getPitch();
+        this.currentPitch = drive.getPitch();
+        this.deltaPitch = 0;
         this.done = false;
     }
 
     public void execute() {
-        double deltaPitch = drive.getPitch() - startingPitch;
+        currentPitch = drive.getPitch();
+        deltaPitch = currentPitch - startingPitch;
         if (Math.abs(deltaPitch) > THRESHOLD) {
             drive.stop();
             done = true;
         } else if (startingPitch > 0) {
             drive.drive(FORWARD);
+            done = false;
         } else {
             drive.drive(BACKWARD);
+            done = false;
         }
     }
 
